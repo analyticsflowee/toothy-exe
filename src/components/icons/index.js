c2d6cae74e11c31f18a4e58a68e10026cb8215a3
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-04-04 18:11:06 +0200

    chore: add more icons

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -2,3 +2,5 @@ export { default as SearchIcon } from "./SearchIcon.vue";
 export { default as CheckIcon } from "./CheckIcon.vue";
 export { default as CrossIcon } from "./CrossIcon.vue";
 export { default as SelectorIcon } from "./SelectorIcon.vue";
+export { default as PlusCircleIcon } from "./PlusCircleIcon.vue";
+export { default as MinusCircleIcon } from "./MinusCircleIcon.vue";
