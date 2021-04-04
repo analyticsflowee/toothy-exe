76a91efdef3faf1f4c6f767052c48495b733d0c2
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-04-04 18:11:18 +0200

    feat: add with-label component

---
+++ src/components/WithLabel.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <label :for="forAttribute" class="block text-md font-bold text-gray-700">
+      {{ label }}
+    </label>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    forAttribute: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
