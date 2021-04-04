d41fc854885a221c554d831e829bfce954570899
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-04-04 13:22:09 +0200

    chore: wire components in app for testing

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <template>
-  <div></div>
+  <PageLayout>
+    <template #left>
+      <SelectDropdown
+        name="cabinet-select"
+        :options="cabinetOptions"
+        v-model="selectedCabinet"
+      />
+      <AddOperators />
+    </template>
+    <template #footer>
+      <BasicButton class="w-full"> Save </BasicButton>
+    </template>
+  </PageLayout>
 </template>
 
 <script>
+import PageLayout from "./components/PageLayout";
+import SelectDropdown from "./components/SelectDropdown";
+import BasicButton from "./components/BasicButton";
+import AddOperators from "./components/AddOperators";
 export default {
   name: "App",
-  components: {},
+  components: {
+    SelectDropdown,
+    PageLayout,
+    BasicButton,
+    AddOperators,
+  },
+  data() {
+    return {
+      selectedCabinet: "",
+    };
+  },
+  computed: {
+    cabinetOptions() {
+      return [1, 2, 3, 4].map((key) => ({ key, label: `Kabinet ${key}` }));
+    },
+  },
 };
 </script>
 
