f0cbe7b6fb8872c13ba96b4b7047161884177ccb
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-04-04 17:14:20 +0200

    refactor: select dropdown to use base dropdown

--- src/components/SelectDropdown.vue
+++ src/components/SelectDropdown.vue
@@ -1,28 +1,39 @@
 <template>
-  <div>
-    <!-- TODO: refactor this to use BasicDropdown -->
-    <label :for="uniqueId" class="block text-sm font-medium text-gray-700">
-      <slot></slot>
-    </label>
-    <select
-      :id="uniqueId"
-      :name="name"
-      class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-      v-model="internalValue"
-    >
-      <option
-        v-for="(option, index) in options"
-        :key="index"
-        :selected="isSelected(option)"
+  <BasicDropdown
+    :options="options"
+    :selected="[value.key]"
+    @select="setSelected"
+    ref="dropdown"
+  >
+    <template #default="{ toggle, hide }">
+      <button
+        type="button"
+        class="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-9"
+        aria-haspopup="listbox"
+        aria-expanded="true"
+        aria-labelledby="listbox-label"
+        @click="toggle"
+        @keydown.esc="hide"
       >
-        {{ option.label }}
-      </option>
-    </select>
-  </div>
+        <span class="block truncate"> {{ value.label }} </span>
+        <span
+          class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none"
+        >
+          <SelectorIcon class="text-gray-400" />
+        </span>
+      </button>
+    </template>
+  </BasicDropdown>
 </template>
 
 <script>
+import BasicDropdown from "./BasicDropdown";
+import { SelectorIcon } from "./icons";
 export default {
+  components: {
+    BasicDropdown,
+    SelectorIcon,
+  },
   props: {
     options: {
       type: Array,
@@ -33,26 +44,14 @@ export default {
       required: true,
     },
     value: {
-      type: String,
-      default: null,
-    },
-  },
-  computed: {
-    uniqueId() {
-      return `select_${this._uid}`;
-    },
-    internalValue: {
-      get() {
-        return this.value;
-      },
-      set(value) {
-        this.$emit("input", value.key);
-      },
+      type: Object,
+      default: () => ({}),
     },
   },
   methods: {
-    isSelected(option) {
-      return option.key === this.value;
+    setSelected(value) {
+      this.$emit("input", value);
+      this.$refs.dropdown.hide();
     },
   },
 };
