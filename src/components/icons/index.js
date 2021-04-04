332d3921761eef9d217b96892d2024d55d74b278
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-04-04 17:13:05 +0200

    chore: add selector icon

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,3 +1,4 @@
 export { default as SearchIcon } from "./SearchIcon.vue";
 export { default as CheckIcon } from "./CheckIcon.vue";
 export { default as CrossIcon } from "./CrossIcon.vue";
+export { default as SelectorIcon } from "./SelectorIcon.vue";
