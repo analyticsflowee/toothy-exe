afa661db2656c3731e61a07cac79b41e1820cbed
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-04-10 16:11:12 +0200

    feat: add process listeners and utilities

--- src/background.js
+++ src/background.js
@@ -1,14 +1,40 @@
 "use strict";
 
-import { app, protocol, BrowserWindow } from "electron";
+import { app, protocol, BrowserWindow, ipcMain } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
+import psList from "ps-list";
+import path from "path";
+import settings from "electron-settings";
+
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: "app", privileges: { secure: true, standard: true } },
 ]);
+
+async function getProcesses() {
+  return await psList();
+}
+
+function cleanCmd({ cmd }) {
+  return cmd.split(" ")[0];
+}
+
+async function scanProcesses(win) {
+  const list = await getProcesses();
+  const toCheck = await settings.get("process");
+  const find = list.find((l) => cleanCmd(l) === cleanCmd(toCheck));
+
+  if (!find) {
+    try {
+      win.show();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
 
 async function createWindow() {
   // Create the browser window.
@@ -33,6 +59,8 @@ async function createWindow() {
     // Load the index.html when not in development
     win.loadURL("app://./index.html");
   }
+
+  return win;
 }
 
 // Quit when all windows are closed.
@@ -44,7 +72,7 @@ app.on("window-all-closed", () => {
   }
 });
 
-app.on("activate", () => {
+app.on("activate", async () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
@@ -62,7 +90,10 @@ app.on("ready", async () => {
       console.error("Vue Devtools failed to install:", e.toString());
     }
   }
-  createWindow();
+  ipcMain.handle("get-processes", getProcesses);
+
+  const win = await createWindow();
+  setInterval(() => scanProcesses(win), 2000);
 });
 
 // Exit cleanly on request from parent process in development mode.
