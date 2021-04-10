6e8794de5288f8bf4da514ca0e827c06733690b0
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-04-10 16:10:37 +0200

    fix: enable listeners on buttons

--- src/components/BasicButton.vue
+++ src/components/BasicButton.vue
@@ -1,5 +1,6 @@
 <template>
   <button
+    v-on="$listeners"
     type="button"
     class="inline-flex items-center justify-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
   >
@@ -8,3 +9,7 @@
     </span>
   </button>
 </template>
+
+<script>
+export default {};
+</script>
