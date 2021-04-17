868816d7810662e5cf0eaeb8b936bfc5e0d18bef
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-04-17 15:51:51 +0200

    chore: add translation system

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import VueI18n from "vue-i18n";
 import { firestorePlugin } from "vuefire";
 import App from "./App.vue";
 import "./assets/tailwind.css";
@@ -7,8 +8,14 @@ import router from "./router";
 Vue.config.productionTip = false;
 
 Vue.use(firestorePlugin);
+Vue.use(VueI18n);
+
+const i18n = new VueI18n({
+  locale: "en",
+});
 
 new Vue({
   router,
+  i18n,
   render: (h) => h(App),
 }).$mount("#app");
