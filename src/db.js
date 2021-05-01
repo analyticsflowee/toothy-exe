58c51cca8b732f027101b4b7491d5f5facdbe5da
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2021-05-01 16:17:39 +0200

    feat: add auth to firestore calls

--- src/db.js
+++ src/db.js
@@ -1,10 +1,19 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/auth";
+
+const password = "electron1234@";
+const email = "electron@toothy.ee";
 
 // Get a Firestore instance
 export const db = firebase
-  .initializeApp({ projectId: "toothy-admin" })
+  .initializeApp({
+    projectId: "toothy-admin",
+    apiKey: "AIzaSyACKcDs-KTSOdNyym9-Bj8Dl44vXuy-kTk",
+  })
   .firestore();
+
+firebase.auth().signInWithEmailAndPassword(email, password);
 
 // Export types that exists in Firestore
 // This is not always necessary, but it's used in other examples
